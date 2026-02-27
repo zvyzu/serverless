d332510157e29f50b2f707d0aa70684acd5f3e31
unknown <zvyz@outlook.com>
2026-02-27 10:24:55 +0700

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -16,8 +16,11 @@ const welcomeStrings = [
 app.get("/api", (c) => {
   return c.text(welcomeStrings.join("\n\n"));
 });
+app.get("/", (c) => {
+  return c.redirect("/index.html");
+});
 // Serve index.html at root
-app.get("/", serveStatic({ path: "./public/index.html" }));
+// app.get("/", serveStatic({ path: "./public/index.html" }));
 
 // Serve other static files
 app.use("/*", serveStatic({ root: "./public" }));
